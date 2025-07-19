5745e9c01eab7c2846c2074d0fd11e7f920e3e7c
Petya228Kvas <167185030+Petya228Kvas@users.noreply.github.com>
2025-07-19 11:01:22 +0500

    Update Server.cpp

--- Server.cpp
+++ Server.cpp
@@ -6,23 +6,91 @@
 #include <thread>
 #include <mutex>
 #include <cstring>
+#include <sstream>
 #pragma warning(disable:4996)
 
 using namespace std;
 
 vector<SOCKET> Users_Connected;
+
 mutex mtx;
 int counter = 0;
 
+string recv(SOCKET client) {
+	int msg_size_recv = 0;
+	recv(client, (char*)&msg_size_recv, sizeof(int), NULL);
+	char* msg_recv = new char[msg_size_recv + 1];
+	recv(client, msg_recv, msg_size_recv, NULL);
+	msg_recv[msg_size_recv] = '\0';
+	string receivedPassword(msg_recv);
+	delete[] msg_recv;
+	return receivedPassword;
+}
 
+void send(SOCKET client, string msg) {
+	size_t msg_size_send = msg.size();
+	send(client, (char*)&msg_size_send, sizeof(int), NULL);
+	send(client, msg.c_str(), msg_size_send, NULL);
+}
+
+
+bool RecvPassword(SOCKET client) {
+	const int maxAttempts = 4;
+	
+	stringstream ss;
+	ss << this_thread::get_id();
+	int id;
+	ss >> id;
+	string id_client = to_string(id);
+
+	string msg = "Hello, to enter the chat enter the password: ";
+	send(client, msg);
+	for (int current = 1; current != maxAttempts; ++current) {
+		if (recv(client) == "ban") {
+			send(client, "Password true! You have access to chat.");
+			send(client, "You have been assigned an id: " + id_client);
+			return true;
+		}
+		else {
+			if (current <= maxAttempts) {
+				int attempt_left = maxAttempts - current;
+				string msg_attempt = to_string(attempt_left) + " attempt(s) left. Try again: ";
+				send(client, msg_attempt);
+			}
+
+		}
+	}
+	send(client, "All attempts are wasted.");
+	return false;
+}
 
 void ClientHandler(SOCKET clientsocket) {
+
+	stringstream ss;
+	ss << this_thread::get_id();
+	int id;
+	ss >> id;
+	string id_client = to_string(id);
+
+	if (!RecvPassword(clientsocket)) {
+		cout << "Authentication failed for " << id_client << ". Closing connection." << endl;
+		closesocket(clientsocket);
+		return;
+	}
+
+	cout << "User " << id_client << " successfully authenticated." << endl;
+
+	mtx.lock();
+	Users_Connected.push_back(clientsocket);
+	counter++;
+	mtx.unlock();
+
 
 	while (true) {
 		int msg_size = 0;
 		int result = recv(clientsocket, (char*)&msg_size, sizeof(int), NULL);//принятие размера сообщения
 		if (result <= 0) {
-			cout << "User " << clientsocket << " disconected or error." << endl;
+			cout << "User " << id_client << " disconected or error." << endl;
 			mtx.lock();
 			for (auto it = Users_Connected.begin(); it != Users_Connected.end(); ++it) {
 				if (*it == clientsocket) {
@@ -39,7 +107,7 @@ void ClientHandler(SOCKET clientsocket) {
 		msg[msg_size] = '\0';// для конца строки
 		result = recv(clientsocket, msg, msg_size, NULL);//Принятие самого сообщения ///////Точка ошибки
 		if (result <= 0) {
-			cout << "User " << clientsocket << " disconected or error." << endl;
+			cout << "User " << id_client << " disconected or error." << endl;
 			mtx.lock();
 			for (auto it = Users_Connected.begin(); it != Users_Connected.end(); ++it) {
 				if (*it == clientsocket) {
@@ -66,44 +134,7 @@ void ClientHandler(SOCKET clientsocket) {
 
 }
 
-string recv(SOCKET client) {
-	int msg_size_recv = 0;
-	recv(client, (char*)&msg_size_recv, sizeof(int), NULL);
-	char* msg_recv = new char[msg_size_recv + 1];
-	recv(client, msg_recv, msg_size_recv, NULL);
-	msg_recv[msg_size_recv] = '\0';
-	string receivedPassword(msg_recv);
-	delete[] msg_recv;
-	return receivedPassword;
-}
 
-void send(SOCKET client, string msg) {
-	size_t msg_size_send = msg.size();
-	send(client, (char*)&msg_size_send, sizeof(int), NULL);
-	send(client, msg.c_str(), msg_size_send, NULL);
-}
-
-bool RecvPassword(SOCKET client) {
-	const int maxAttempts = 4;
-	string msg = "Hello, to enter the chat enter the password: ";
-	send(client, msg);
-	for (int current = 1; current != maxAttempts; ++current) {
-		if (recv(client) == "ban") {
-			send(client, "Password true! You have access to chat.");
-			return true;
-		}
-		else {
-			if (current <= maxAttempts) {
-				int attempt_left = maxAttempts - current;
-				string msg_attempt = to_string(attempt_left) + " attempt(s) left. Try again: ";
-				send(client, msg_attempt);
-			}
-			
-		}
-	}
-	send(client, "All attempts are wasted.");
-	return false;
-}
 
 
 
@@ -132,25 +163,20 @@ int main() {
 
 	SOCKET newconnection;
 	vector<thread> client_threads;
+	
 	while (true) {
 		newconnection = accept(slisten, (SOCKADDR*)&addr, &size_of_len);
 		
-		if (!RecvPassword(newconnection)) {
-			closesocket(newconnection);
-		}
-		else {
+		
+		
 
-			if (newconnection == INVALID_SOCKET)
-				cout << "User is not connected." << endl;
-			else {
-				cout << "New User connected." << endl;
-				mtx.lock();
-				Users_Connected.push_back(newconnection);
-				counter++;
-				mtx.unlock();
-				client_threads.emplace_back(ClientHandler, newconnection);
-				client_threads.back().detach();
-			}
+		if (newconnection == INVALID_SOCKET)
+			cout << "User is not connected." << endl;
+		else {
+			cout << "New User attempting to connect." << endl;
+		
+			client_threads.emplace_back(ClientHandler, newconnection);
+			client_threads.back().detach();
 		}
 	}
 
